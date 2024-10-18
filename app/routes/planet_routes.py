fa09142bec80e54b9ede9098cf0eba8efb2f978d
mayboolean <>
2024-10-18 15:03:40 -0400

    wave 2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -15,3 +15,21 @@ def get_all_planets():
             size=planet.size
         ))
     return results_list
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return {"message": f"planet {planet_id} is an invalid ID"}, 400
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return dict(
+                id=planet.id,
+                name=planet.name,
+                desc=planet.description,
+                size=planet.size
+            )
+    else:
+        return {"message": f"planet {planet_id} not found"}, 404
