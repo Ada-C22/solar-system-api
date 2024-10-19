9e61c2d57c26c8dfcc055d10ea2ce2850b761444
Leidy <leidymarbu@gmail.com>
2024-10-19 19:37:59 -0300

    fisrt code for wave 2

--- app/routes/planet_route.py
+++ app/routes/planet_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from app.models.planets import planets
+from flask import Blueprint, abort, make_response
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
@@ -14,5 +15,29 @@ def get_all_planets():
                 "description":planet.description  
             }
         )
-
     return planets_list
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet_id(planet_id)
+
+    return {
+        "id": planet.id,
+        "title": planet.name,
+        "description": planet.description,
+        "distance_from_sun": planet.distance_from_sun
+    }
+
+def validate_planet_id(planet_id):
+    try:
+        planet_id = int(planet_id) 
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
