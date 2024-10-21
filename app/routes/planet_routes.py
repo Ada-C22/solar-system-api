0b2d368d8ab9a2893b6979adc506b19031441a7c
Salma Anany <salmayousry5@gmail.com>
2024-10-20 18:54:40 -0700

    clean up the try and catch, to a if check

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,7 +1,5 @@
-
 from flask import Blueprint, abort, make_response
 from app.models import planets
-# from ..models.planet import Planet
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
@@ -9,21 +7,16 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 def get_all_planet():
     result_list = []
-
     for planet in planets:
         result_list.append(planet.__dict__)
     return result_list
 
 
 @planets_bp.get("/<planet_id>")
-
 def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
+    if not planet_id.isnumeric():
         response = {"message": f"Planet {planet_id} invalid"}
         abort(make_response(response, 400))
-
     for planet in planets:
         if planet.id == planet_id:
             return planet.__dict__
