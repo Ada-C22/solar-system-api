5947424a1271bdb4b8806bd2ba2f13ebdfc60e2f
Salma Anany <salmayousry5@gmail.com>
2024-10-21 12:26:37 -0700

    Wave Two completed

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -7,7 +7,6 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 def get_all_planet():
     result_list = []
     for planet in planets:
-        # .__dict__ to return any object to dict
         result_list.append(planet.__dict__)
     return result_list
 
@@ -33,14 +32,3 @@ def validate_planet(planet_id):
     abort(make_response({"message": f"planet {planet_id} not found"}, 404))
 
 
-
-    # if not planet_id.isnumeric():
-    #     response = {"message": f"Planet {planet_id} invalid"}
-    #     abort(make_response(response, 400))
-    #
-    # for planet in planets:
-    #     if planet.id == int(planet_id):
-    #         return planet.__dict__
-    #
-    # response = {"message": f"planet {planet_id} not found"}
-    # abort(make_response(response, 404))
