fd7ad48557971c5f59ceda08855cee5d640feded
Natalie <natali3sen@gmail.com>
2024-10-23 19:39:39 -0500

    rearrange planet list

--- app/models/planets.py
+++ app/models/planets.py
@@ -9,12 +9,14 @@ class Planet:
 
 
 planet_list = [
-    Planet(21, "Mercury", "hottest", "56325"),
-    Planet(32, "Saturn", "with_rings", "653645"),
-    Planet(45, "Venus", "small", "657436"),
-    Planet(56, "Mars", "lively", "765347"),
-    Planet(78, "Pluto", "non_existing", "763787"),
-    Planet(89, "Jupiter", "biggest", "98765")
+    Planet(21, "Mercury", "hottest", "3,032"),
+    Planet(32, "Saturn", "with_rings", "74,897"),
+    Planet(45, "Venus", "small", "7,521"),
+    Planet(56, "Mars", "lively", "4,212"),
+    Planet(78, "Pluto", "non_existing", "1,477"),
+    Planet(89, "Jupiter", "biggest", "88,846"),
+    Planet(77, "Earth", "have_lives", "7,926"),
+    Planet(90, "Neptune", "big_and_blue", "30,775"),
+    Planet(85, "Uranus", "icy", "31,763 ")
 ]
-
 
