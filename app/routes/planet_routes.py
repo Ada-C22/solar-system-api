64eec780b5bf05344751070352bc9792fc82f5c3
Nikki Vaughan <vnikki13@gmail.com>
2025-01-23 17:11:05 -0500

    Complete wave 4: read, update, and delete planets.

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
 
@@ -21,6 +21,29 @@ def get_all_planets():
     )
   return planet_response
 
+@planet_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+  planet = validate_planet(planet_id)  
+
+  return {
+    "name": planet.name,
+    "description": planet.description,
+    "diameter": planet.diameter,
+  }
+
+@planet_bp.put("/<planet_id>")
+def update_planet(planet_id):
+  planet = validate_planet(planet_id)
+  request_body = request.get_json()
+
+  planet.name = request_body["name"]
+  planet.description = request_body["description"]
+  planet.diameter = request_body["diameter"]
+
+  db.session.commit()
+
+  return Response(status=204, mimetype="application/json")
+
 @planet_bp.post("")
 def create_planet():
   request_body = request.get_json()
@@ -41,5 +64,26 @@ def create_planet():
 
   return response, 201
 
+@planet_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+  planet = validate_planet(planet_id)
+  db.session.delete(planet)
+  db.session.commit()
 
+  return Response(status=204, mimetype="application/json")
 
+def validate_planet(planet_id):
+  try:
+    planet_id = int(planet_id)
+  except:
+    response = {"message": f"Planet {planet_id} invalid"}
+    abort(make_response(response, 400))
+
+  query = db.select(Planet).where(Planet.id == planet_id)
+  planet = db.session.scalar(query)
+
+  if not planet:
+    response = {"message": f"Planet {planet_id} not found"}
+    abort(make_response(response, 404))
+
+  return planet
